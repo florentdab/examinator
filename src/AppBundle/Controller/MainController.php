9e318f17573c7f9fcfc5f8e3c10113a451308f2e
ngdo-pro <ngdo27@gmail.com>
2017-01-21 10:23:51 +0100

    exercices done

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -8,6 +8,50 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('app/main/index.html.twig');
+        $ex1Entry = array(
+            array(
+                124
+            ),
+            array(
+                53
+            ),
+            array(
+                175
+            )
+        );
+        $ex1Exit = array(
+            "rgb(124, 53, 175)"
+        );
+
+        $ex2Entry = array(
+            "Kingslayer"
+        );
+
+        $ex2Exit = array(
+            "Lannister" => array(
+                "Cersei as Queen Regent of the Seven Kingdoms",
+            ),
+            "Stark" => array(
+                "Arya as A girl",
+                "Bran as Prince of Winterfell"
+            ),
+            "Targaryen" => array(
+                "Daenerys as Mother of dragons"
+            ),
+            "Greyjoy" => array(
+                "Theon as Prince of Pyk"
+            ),
+            "Martell" => array(
+                "Oberyn as Red Viper",
+                "Doran as Prince of Dorne"
+            )
+        );
+
+        return $this->render('app/main/index.html.twig', array(
+            'ex1Entry'  => $ex1Entry,
+            'ex1Exit'   => $ex1Exit,
+            'ex2Entry'  => $ex2Entry,
+            'ex2Exit'   => $ex2Exit
+        ));
     }
 }
